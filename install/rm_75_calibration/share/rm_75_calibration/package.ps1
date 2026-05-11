432e523ba262f9b0eff28c96a86089b547fa92ea
Q-cy <18069863462@163.com>
2026-05-11 13:29:34 +0800

    yaml

--- install/rm_75_calibration/share/rm_75_calibration/package.ps1
+++ install/rm_75_calibration/share/rm_75_calibration/package.ps1
@@ -112,5 +112,6 @@ $env:COLCON_CURRENT_PREFIX=(Get-Item $PSCommandPath).Directory.Parent.Parent.Ful
 
 colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/rm_75_calibration/hook/pythonpath.ps1"
 colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/rm_75_calibration/hook/ament_prefix_path.ps1"
+colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\../../build/rm_75_calibration/share/rm_75_calibration/hook/pythonpath_develop.ps1"
 
 Remove-Item Env:\COLCON_CURRENT_PREFIX
--- install/rm_75_calibration/share/rm_75_calibration/package.sh
+++ install/rm_75_calibration/share/rm_75_calibration/package.sh
@@ -80,6 +80,7 @@ _colcon_package_sh_source_script() {
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/rm_75_calibration/hook/pythonpath.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/rm_75_calibration/hook/ament_prefix_path.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/../../build/rm_75_calibration/share/rm_75_calibration/hook/pythonpath_develop.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
